694f9b1a8dd373a87d3de29f57376eb6d6f4f234
Jawad Sher <jawadsher04@gmail.com>
2025-01-28 16:49:41 +0500

    update auth route to include versioning in the API path

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import authRouter from "./routes/auth.router.js"
 const app = express();
 const PORT = process.env.PORT || 3000
 
-app.use("/api/auth/", authRouter);
+app.use("/api/v1/auth/", authRouter);
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT: ", + PORT);
