2efd11e48959cf550149ca8095305181b122b569
Jawad Sher <jawadsher04@gmail.com>
2025-01-30 10:41:48 +0500

    feat(video): add video model with aggregation pagination support

---
+++ backend/src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    title: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    description: {
+        type: String,
+        maxLength: 1000
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    views: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        required: true,
+        default: true,
+    }    
+
+}, {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoSchema);
